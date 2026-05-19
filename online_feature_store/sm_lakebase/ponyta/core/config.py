30909d16b39334f6995fec7f146989bdb59fdba1
shuming peh <shuming.peh@deferit.com>
2026-05-19 18:47:44 +1000

    Add skeleton files

---
+++ online_feature_store/sm_lakebase/ponyta/core/config.py
@@ -0,0 +1,47 @@
+from typing import Optional
+
+from dotenv import load_dotenv
+from pydantic import Field
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+
+    AWS_DEFAULT_REGION: str
+    AWS_DEFAULT_REGION = "us-east-1"
+    DATABRICKS_CLUSTER_HOST: Optional[str] = Field(
+        default=None,
+        env="DATABRICKS_HOST",
+    )
+    DATABRICKS_TOKEN: Optional[str] = Field(
+        default=None,
+        env="DATABRICKS_TOKEN",
+    )
+    DATABRICKS_SQL_CLUSTER_PATH: Optional[str] = Field(
+        default=None,
+        env="DATABRICKS_SQL_PATH",
+    )
+    PIPELINE_TYPE: Optional[str] = Field(
+        default="TRIGGERED",
+        env="PIPELINE_TYPE",
+    )
+    UNITY_CATALOG: Optional[str] = Field(
+        default=None,
+        env="UNITY_CATALOG",
+    )
+    DATABRICKS_CLIENT_ID: Optional[str] = Field(
+        default=None,
+        env="DATABRICKS_CLIENT_ID",
+    )
+    DATABRICKS_CLIENT_SECRET: Optional[str] = Field(
+        default=None,
+        env="DATABRICKS_CLIENT_SECRET",
+    )
+    VECTOR_SEARCH_PREFIX: str
+    VECTOR_SEARCH_PREFIX = "pidgey"
+    VS_ENDPOINT_TYPE: str
+    VS_ENDPOINT_TYPE = "STANDARD"
+
+
+load_dotenv()
+settings = Settings()
